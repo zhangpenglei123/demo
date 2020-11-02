7b8edda47f76fded79c0709ec8287757a7feb96a
root <1374525523@qq.com>
2020-11-02 22:42:44 +0800

    springboot工程初次成立

---
+++ src/main/java/com/example/develop/DevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopApplication.class, args);
+    }
+
+}
